731f24e9b6d2a96cc4cc121c854a3edae207669f
shixin <shixin58@163.com>
2018-04-13 18:42:13 +0800

    RoundCornerImageView加边框

--- app/src/main/java/com/victor/utils/widget/RoundCornerImageView.java
+++ app/src/main/java/com/victor/utils/widget/RoundCornerImageView.java
@@ -23,6 +23,7 @@ public class RoundCornerImageView extends AppCompatImageView {
     private RectF rectF = new RectF();
     private float[] radii = new float[8];
     private DrawFilter drawFilter = new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG| Paint.FILTER_BITMAP_FLAG);
+    private float mStrokeWidth = 0;
 
     public RoundCornerImageView(Context context) {
         super(context);
@@ -32,17 +33,20 @@ public class RoundCornerImageView extends AppCompatImageView {
         super(context, attrs);
         if (attrs != null) {
             TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.RoundCornerImageView, 0, 0);
+            // 设置圆角
             float radius = a.getDimension(R.styleable.RoundCornerImageView_corner_radius, 0f);
             for(int i=0;i<radii.length;i++) {
                 radii[i] = radius;
             }
+            // 设置边框
+            mStrokeWidth = a.getDimension(R.styleable.RoundCornerImageView_stroke_width, 0f);
             a.recycle();
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        rectF.set(0, 0, this.getWidth(), this.getHeight());
+        rectF.set(mStrokeWidth, mStrokeWidth, this.getWidth()- mStrokeWidth, this.getHeight()- mStrokeWidth);
         path.addRoundRect(rectF, radii, Path.Direction.CW);
         canvas.setDrawFilter(drawFilter);
         canvas.clipPath(path);
