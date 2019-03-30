621cf8b45cc4c8e3c501eac1ed5d011ca65d31e7
shixin <shixin58@163.com>
2019-03-30 23:23:10 +0800

    review ViewPager

--- app/src/main/java/com/roy/devil/activities/CustomWidgetActivity.java
+++ app/src/main/java/com/roy/devil/activities/CustomWidgetActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 
 import com.bride.baselib.BaseActivity;
 import com.roy.devil.R;
+import com.roy.devil.adapter.BannerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.viewpager.widget.ViewPager;
 
 /**
  * <p>Created by shixin on 2018/4/5.
@@ -14,5 +20,19 @@ public class CustomWidgetActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_widget);
+        initView();
+    }
+
+    private void initView() {
+        ViewPager viewPager = findViewById(R.id.banner);
+        BannerAdapter bannerAdapter = new BannerAdapter();
+        viewPager.setAdapter(bannerAdapter);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(R.drawable.desert);
+        list.add(R.drawable.mojave);
+        list.add(R.drawable.sierra);
+        list.add(R.drawable.sailboat_race);
+        bannerAdapter.setList(list);
     }
 }
---
+++ app/src/main/java/com/roy/devil/adapter/BannerAdapter.java
@@ -0,0 +1,64 @@
+package com.roy.devil.adapter;
+
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.roy.devil.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+/**
+ * <p>Created by shixin on 2019/3/30.
+ */
+public class BannerAdapter extends PagerAdapter {
+    private List<Integer> mList = new ArrayList<>();
+    private SparseArray<ImageView> mImageViews = new SparseArray<>();
+
+    public void setList(List<Integer> list) {
+        if (list != null && !list.isEmpty()) {
+            mList.clear();
+            mList.addAll(list);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        position = position % mList.size();
+        ImageView imageView = mImageViews.get(position);
+        if (imageView == null) {
+            imageView = (ImageView) LayoutInflater.from(container.getContext())
+                    .inflate(R.layout.banner_item, container, false);
+            mImageViews.put(position, imageView);
+            Glide.with(container.getContext())
+                    .load(mList.get(position))
+                    .into(imageView);
+        }
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
